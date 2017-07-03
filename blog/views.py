c59610403fe00ba6780d3eb245482087c2fe6b2d
Caitlin <caitlinchou@gmail.com>
2017-07-03 07:58:24 -0400

    add render shortcut

--- blog/views.py
+++ blog/views.py
@@ -4,23 +4,22 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 from .models import Review
 
 
 def main(request):
     latest_review_list = Review.objects.order_by('-date')
-    template = loader.get_template('blog/main.html')
     context = {
         'latest_review_list': latest_review_list,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'blog/main.html', context)
 
 
 def detail(request, review_id):
-    template = loader.get_template('blog/detail.html')
     context = {
         'review': Review.objects.get(pk=review_id),
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'blog/detail.html', context)
