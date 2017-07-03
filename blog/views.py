8989adae9d9519c94a89a0d82cb560903539defe
Caitlin <caitlinchou@gmail.com>
2017-07-03 07:10:24 -0400

    plural fix

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Review
 
 
 def main(request):
-    return HttpResponse("This is the blog")
+    latest_review_list = Review.objects.order_by('-date')
+    template = loader.get_template('blog/main.html')
+    context = {
+        'latest_review_list': latest_review_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
-def detail(request, product_id):
-    return HttpResponse("This is product " + product_id)
+def detail(request, review_id):
+    template = loader.get_template('blog/detail.html')
+    context = {
+        'review': Review.objects.get(pk=review_id),
+    }
+    return HttpResponse(template.render(context, request))
