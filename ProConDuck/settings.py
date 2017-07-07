5e8186bec47cde020a5f1d8c41a05b5b64c9f39c
Caitlin <caitlinchou@gmail.com>
2017-07-07 19:53:26 -0400

    edit some file names

--- ProConDuck/settings.py
+++ ProConDuck/settings.py
@@ -33,14 +33,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['quiet-beyond-56572.herokuapp.com',
                  'www.proconduck.com',
                  '127.0.0.1',
                  'testserver',
                  ]
-
 
 
 # Application definition
