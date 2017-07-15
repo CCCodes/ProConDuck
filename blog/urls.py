c24bfec2137d604187b87ea3c766e7fc3bda531f
alisonchou <myallychou@Gmail.com>
2017-07-15 19:40:05 -0400

    fix URLS

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^(?P<review_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^contact-us', views.contact),
+    url(r'^contact-us/', views.contact),
 ]
 
 handler404 = views.error404
