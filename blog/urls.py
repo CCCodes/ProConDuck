74e793753e3f392085dd91b4f94bc8fad8a600d1
alison.chou <myallychou@gmail.com>
2017-08-07 09:49:30 -0400

    change titles, fix privacy policy, add about page

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^contact-us/submit/$', views.contact_submit, name='contact_submit'),
     url(r'^terms-of-service/$', views.tos, name="tos"),
     url(r'^privacy-policy/$', views.privacy, name="privacy"),
+    url(r'^about/$', views.about, name="about"),
     url(r'^signup/?(?P<success>[-\w\d]+)/?$', views.signup, name='signup'),
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^error404/$', views.error404, name='error404'),
