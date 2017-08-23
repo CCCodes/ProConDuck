f26252d03a856f075053eeb928bb276b4ad4ea33
Caitlin <caitlinchou@gmail.com>
2017-08-23 13:32:50 -0400

    add cache middleware

--- ProConDuck/settings.py
+++ ProConDuck/settings.py
@@ -65,6 +65,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'ProConDuck.urls'
