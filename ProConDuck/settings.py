ead3b2f5c32626feb4e4ec21fa8f831a05d789a0
CCCodes <caitlinchou@gmail.com>
2021-01-03 22:04:37 -0500

    new Django secret key

--- ProConDuck/settings.py
+++ ProConDuck/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = config['NEW_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = THUMBNAIL_DEBUG = False
