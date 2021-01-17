c4ce19e62d5b50b3da9d258fa4e40831e159f2f7
CCCodes <caitlinchou@gmail.com>
2021-01-17 11:49:37 -0500

    work around email

--- ProConDuck/settings.py
+++ ProConDuck/settings.py
@@ -183,7 +183,6 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
-print(EMAIL_HOST_PASSWORD)
 EMAIL_HOST_USER = 'proconduck@gmail.com'
 EMAIL_PORT = 587
 
